61e7f27f1ddb050316459f71f04e7c942b9f17d9
Ricardo Daniel Romero <Richard@Ricardos-MacBook-Pro.local>
2020-05-31 17:28:01 -0300

    Front de Richard

--- public/js/app.js
+++ public/js/app.js
@@ -19335,8 +19335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Argentina\Desktop\proyect-OnePage-Alfredo\onePage-Alfredo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Argentina\Desktop\proyect-OnePage-Alfredo\onePage-Alfredo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/Richard/Documents/laburo/web/onePage-responsive/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/Richard/Documents/laburo/web/onePage-responsive/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,37 +9,40 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Monserrat:200,600" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="icon" type="image/ico" href="img/favicon.ico">   
     </head>
     <body>
        <div class="container-fluid">
             <div class="row" style="background-color: #1c4c84"> 
                 <div class="col-12 col-md-6">
-                    <img class="mt-5 ml-5" src="img/Logo-principal.jpg" alt="logo">
-                    <h1 class="mt-4 ml-5" style="font-size:4em;color:white;">TU CARRERA <br> UNIVERSITARIA <br></h1>
-                    <h1 class="ml-5" style="font-size:4em;color:#f7ca48;">EN ISRAEL</h1>
-                    <p class="mt-3 ml-5" style="font-size:1.5em;color:white;">Megamat Aliá es la oortunidad de acceder <br> a <strong>las mejores unirvesidades</strong> del Estado de Israel </p>
-                    <button class="mt-3 mb-4 ml-5 btn" style="color:#1c4c84;background-color:#f7ca48;border:none;">CONOCÉ MÁS  </button>
+                <img class="ml-lg-5 mt-5" src="img/Logo-principal.jpg" alt="logo">
+                    <h1 class="mt-4 ml-lg-5 ti1">TU CARRERA </h1>
+                    <h1 class="ml-lg-5 ti1">UNIVERSITARIA</h1>
+                    <h1 class="ml-lg-5 ti1" style="color:#f7ca48;">EN ISRAEL</h1>
+                    <p class="mt-3 ml-lg-5 ba" style="font-size:1.5em;color:white;">Megamat Aliá es la oortunidad de acceder <br> a <strong>las mejores unirvesidades</strong> del Estado de Israel </p>
+                    <button class="mt-3 mb-4 ml-lg-5 btn ba" style="color:#1c4c84;background-color:#f7ca48;border:none;font-weight: 900;">CONOCÉ MÁS <i class="fa fa-chevron-down"></i> </button>
                 </div>
-                <div class="col-md-6 ">
-                    <img src="img/imagen-principal.jpg" alt="img principal">
+                <div class="col-md-6" style="overflow: hidden;">
+                    <img style="min-height: 100%;min-width: 100%;" src="img/imagen-principal.jpg" alt="img principal">
                 </div>
             </div>
             <div class="row mt-3 mb-4">
-                <h4 class="mt-3 offset-md-3 col-md-8" style="color: #1c4c84">ENVIANOS TUS DATOS PARA INSCRIBIRTE AL PROGRAMA</h4>
+                <h4 class="mt-3 offset-md-2 offset-xl-3 col-md-8 ba" style="color: #1c4c84">ENVIANOS TUS DATOS PARA INSCRIBIRTE AL PROGRAMA</h4>
                 <form class="mx-auto mt-4 mb-5 text-center" method="POST" action="/envioDatos">
                     @csrf
-                    <div class="form-row mb-3 ">
+                    <div class="form-row mb-3 mw-100">
                       <div class="form-group col-md-4 mt-2" >
                         <input type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre" autofocus  placeholder="Nombre" >
                       </div>
                       <div class="form-group col-md-4 mt-2 ">
                         <input type="text" class="form-control  placeholder="Apellidos">
                       </div>
-                      <div class="form-group col-md-4 mt-2  ">
+                      <div class="form-group col-md-4 mt-2 ">
                         <input type="date" class="form-control" id="inputPassword4" placeholder="Fecha de nacimiento">
                       </div>
                     </div>
-                    <div class="form-row mb-3 ">
+                    <div class="form-row mb-3 mw-100">
                         <div class="form-group col-md-4 mt-2">
                           <input type="number" class="form-control" id="inputEmail4" placeholder="DNI">
                         </div>
@@ -50,7 +53,7 @@
                           <input type="number" class="form-control" id="inputPassword4" placeholder="Teléfono">
                         </div>
                       </div>
-                      <div class="form-row mb-3 ">
+                      <div class="form-row mb-3 mw-100">
                         <div class="form-group col-md-4 mt-2">
                           <input type="text" class="form-control" id="inputEmail4" placeholder="Dirección">
                         </div>
@@ -61,11 +64,11 @@
                           <input type="text" class="form-control" id="inputPassword4" placeholder="Provincia">
                         </div>
                       </div>
-                      <button type="submit" class="btn" style="background-color:#1c4c84;color:#f7ca48;border:none">ENVIAR</button>
+                      <button type="submit" class="btn ba" style="background-color:#1c4c84;color:#f7ca48;border:none;font-weight: 800;">ENVIAR</button>
                   </form>
             </div>
             <div class="row mt-3 py-3" style="background-color: #1c4c84">
-                    <img class="mx-auto" src="img/logo-pie1.jpg" alt="cargando img" style="height:0.5%">
+                    <img class="mw-100 mx-auto" src="img/logo-pie1.jpg" alt="cargando img" style="height:0.5%">
                     <img class="mx-auto" src="img/logo-pie2.jpg" alt="cargando img" style="height:0.5%">
                     <img class="mx-auto" src="img/logo-pie3.jpg" alt="cargando img" style="height:0.5%">
                     <img class="mx-auto" src="img/logo-pie4.jpg" alt="cargando img" style="height:0.5%">
